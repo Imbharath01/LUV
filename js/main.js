c5b4cb7b2293d8c4f379f2a688eba80f3d2f6423
Always Distracted <padhmanabbanbascarane@gmail.com>
2025-02-16 22:03:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I Love You Soooooooo Muchhhhhhhh Ammuuuuuuuuuuu').split('')
+    const titles = ('I Love You Soooooooo Muchhhhhhhh Ammuuuuuuuuuuu and you are the only person I care sooooooo muchhhhhhhh').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
