56f8be6a247c8475723160158cfe9509e80124bd
Always Distracted <padhmanabbanbascarane@gmail.com>
2025-02-16 22:05:14 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I WANNA TELL YOU SOMETHING AMMUU........!!!!'.split('')
+const text = 'I WANNA TELL YOU SOMETHING AMMUU !!!!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
